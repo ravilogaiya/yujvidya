a4c4456fe0402dd92110cd9bb92cd6046d3a3596
rlogaiy <rlogaiy@RLOGAIY-IN-LE04>
2017-07-08 00:11:24 +0530

    Added miissed file

---
+++ docs/yujvidya.env.sh
@@ -0,0 +1,9 @@
+echo "setting envoirnment variables for yujvidya"
+
+export YV_DB_USERID=<replace with your value>
+export YV_DB_PASSWORD=<replace with your value>
+
+export YV_SMS_USER=<replace with your value>
+export YV_SMS_PASSWORD<replace with your value>
+export YV_SMS_SENDER=<replace with your value>
+
